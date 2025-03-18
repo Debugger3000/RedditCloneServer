0b88d59e58c8e899c8aff41a0fec7cf3c8d396f2
Debugger3000 <carmac.ford@gmail.com>
2025-03-17 22:11:31 -0400

    mongoose connection / user model created

--- models/users.js
+++ models/users.js
@@ -1,1 +1,29 @@
-import { Mongoose } from "mongoose";
+// import { Mongoose } from "mongoose";
+
+// Require the mongoose module 
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema;
+
+const userSchemaObject = {
+    username: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      password: {
+        type: String,
+        required: true
+      }
+}
+
+const mongooseSchema = mongoose.Schema(userSchemaObject);
+
+module.exports = mongoose.model('User',mongooseSchema);
+  
+// Defining User model 
+// export const User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 // initialize base of express app
 const app = express();
@@ -11,6 +12,15 @@ app.use(express.json());
 app.use(cookieParser());
 
 // "type": "module",
+
+// mongodb+srv://Carter:weBHook34^5@redditclonecartier.8eovm.mongodb.net/?retryWrites=true&w=majority&appName=RedditCloneCartier
+//mongo connection
+mongoose
+.connect('mongodb+srv://Carter:weBHook34^5@redditclonecartier.8eovm.mongodb.net/?retryWrites=true&w=majority&appName=RedditCloneCartier')
+.then(() => { console.log('connected to mongo DB for Reddit clone !');
+})
+.catch((err) => {console.log('Error:',err);
+});
 
 
 // Routes
