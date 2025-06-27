b3dc713b93a0da366d3f30db683b23fb957fa394
Debugger3000 <carmac.ford@gmail.com>
2025-06-26 22:02:06 -0400

    votes

--- routes/post.js
+++ routes/post.js
@@ -1,17 +1,26 @@
-import express from 'express';
+import express from "express";
 
-//main router to 
+//main router to
 const router = express.Router();
 
 // import controller functions
-import { createPost, getPosts, getPostsForThread, getPost, deletePost } from '../controllers/posts.js';
+import {
+  createPost,
+  getPosts,
+  getPostsForThread,
+  getPost,
+  deletePost,
+  vote,
+} from "../controllers/posts.js";
 
 // Post a new thread to collection
-router.post('/', createPost);
-router.get('/', getPosts);
-router.get('/:id', getPostsForThread);
-router.get('/single/:id', getPost);
-router.delete('/:id', deletePost);
+router.post("/", createPost);
+router.get("/", getPosts);
+router.get("/:id", getPostsForThread);
+router.get("/single/:id", getPost);
+router.delete("/:id", deletePost);
 
+// votes
+router.post("/vote", vote);
 
 export default router;
--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 // -------------------------
 // general server imports
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
-import expressSession from 'express-session';
-import connectMongo from 'connect-mongo';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import expressSession from "express-session";
+import connectMongo from "connect-mongo";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import passport from './middleware/Authentication.js';
+import passport from "./middleware/Authentication.js";
 
 // Routes import
-import userRouter from './routes/user.js';
-import threadRouter from './routes/threads.js';
-import postRouter from './routes/post.js';
-import commentRouter from './routes/comment.js'
+import userRouter from "./routes/user.js";
+import threadRouter from "./routes/threads.js";
+import postRouter from "./routes/post.js";
+import commentRouter from "./routes/comment.js";
 // github
-
 
 // -----------------
 // Set environment files based on script start
-dotenv.config({path: `./.env.${process.env.NODE_ENV}`});
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 // log out environment type
-console.log("Environment: ",process.env.NODE_ENV);
-console.log("origin: ",process.env.ORIGIN);
-
+console.log("Environment: ", process.env.NODE_ENV);
+console.log("origin: ", process.env.ORIGIN);
 
 // general variables
 const port = 8080;
@@ -32,89 +30,76 @@ const port = 8080;
 // initialize base of express app
 const app = express();
 
-
 // some middle ware for app to use
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: process.env.ORIGIN,
-    credentials: true
-
-  }
-));
+    credentials: true,
+  })
+);
 
 // Sessions
 const sessionObject = {
-    store: connectMongo.create({
-        mongoUrl: `${process.env.MONGO_CONNECTION}`,
-        //touchAfter: 24 * 3600, // update session when touched only once every 24 hours
-        collectionName: 'RedditCloneSessions',
-    }),
-    name: 'RedditCloneCookie',
-    
-    secret: 'fakeSecret',
-    saveUninitialized: false,
-    resave: false,
-    // sameSite: 'none',
-    // secure: true,
-    // // httpOnly: true,
-    // partitioned: true,
-    
+  store: connectMongo.create({
+    mongoUrl: `${process.env.MONGO_CONNECTION}`,
+    //touchAfter: 24 * 3600, // update session when touched only once every 24 hours
+    collectionName: "RedditCloneSessions",
+  }),
+  name: "RedditCloneCookie",
 
-    cookie: {
-      httpOnly: false,
-      maxAge: 3600000
-    }
-}
+  secret: "fakeSecret",
+  saveUninitialized: false,
+  resave: false,
+  // sameSite: 'none',
+  // secure: true,
+  // // httpOnly: true,
+  // partitioned: true,
+
+  cookie: {
+    httpOnly: false,
+    maxAge: 3600000,
+  },
+};
 app.use(expressSession(sessionObject));
 
-
 app.use(passport.initialize());
-// 
+//
 app.use(passport.session());
 
-
-
-
-
-
-
-passport.serializeUser((user,done) => {
+passport.serializeUser((user, done) => {
   console.log("Serializing");
-  done(null,user);
+  done(null, user);
 });
 
-passport.deserializeUser((user,done) => {
-  console.log('Deserializing');
-  done(null,user);
-})
-
-
-
-
-
+passport.deserializeUser((user, done) => {
+  console.log("Deserializing");
+  done(null, user);
+});
 
 // MONGO DB connection...
 // mongodb+srv://Carter:weBHook34^5@redditclonecartier.8eovm.mongodb.net/?retryWrites=true&w=majority&appName=RedditCloneCartier
 //mongo connection
 mongoose
-.connect(process.env.MONGO_CONNECTION)
-.then(() => { console.log('connected to mongo DB for Reddit clone !');})
-.catch((err) => {console.log('Error:',err);});
-
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    console.log("connected to mongo DB for Reddit clone !");
+  })
+  .catch((err) => {
+    console.log("Error:", err);
+  });
 
 // Routes
-app.use('/api/user', userRouter);
-app.use('/api/thread', threadRouter);
-app.use('/api/post', postRouter);
-app.use('/api/comment', commentRouter);
+app.use("/api/user", userRouter);
+app.use("/api/thread", threadRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
 // app.use('/auth', githubRouter);
 
 // listen to a port
 app.listen(port, () => {
-    console.log("Listening on port 8080");
+  console.log("Listening on port 8080");
 });
 
-
-export { passport }
+export { passport };
