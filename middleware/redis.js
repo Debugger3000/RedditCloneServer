b8b019a50324f3bfcfb53e8672eec9f5889dc773
Debugger3000 <carmac.ford@gmail.com>
2025-07-31 20:35:41 -0400

    firebase signed URLs for secure upload

--- middleware/redis.js
+++ middleware/redis.js
@@ -10,3 +10,20 @@ export const client = await createClient({
     console.log("Local Redis client is ready !");
   })
   .connect();
+
+// get cache data function
+export async function getCacheData(redisKey) {
+  const cached = await client.get(redisKey);
+  // console.log("cached data: ", cached);
+  return JSON.parse(cached);
+}
+
+// set cache data
+export async function setCacheData(redisKey, data) {
+  // expiry - 1 hour
+  const expiry = 3600;
+  const cached = await client.set(redisKey, JSON.stringify(data), {
+    EX: expiry,
+  });
+  return cached;
+}
